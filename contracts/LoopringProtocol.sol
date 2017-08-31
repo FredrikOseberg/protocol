0592d9e96da9bfd4865aff3809f04f95f80374ed
Kongliang Zhong <zkl_1011@126.com>
2017-08-31 22:50:43 +0800

    temporary commit

--- contracts/LoopringProtocol.sol
+++ contracts/LoopringProtocol.sol
@@ -20,6 +20,7 @@ pragma solidity ^0.4.11;
 import "zeppelin-solidity/contracts/token/ERC20.sol";
 import "zeppelin-solidity/contracts/math/Math.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "./TokenRegistry.sol";
 
 /// @title Loopring Token Exchange Contract
 /// @author Kongliang Zhong - <kongliang@loopring.org>, Daniel Wang - <daniel@loopring.org>.
@@ -28,7 +29,11 @@ contract LoopringProtocol {
     using Math for uint;
 
     uint    public constant MAX_RING_SIZE = 5;
-    uint    public   ringIndex = 0;
+    uint    public constant MIN_FILL_AMOUNT = 1 finney;
+    uint    public ringIndex = 0;
+
+    address public LRC_TOKEN_CONTRACT_ADDRESS;
+    address public TOKEN_REGISTRY_CONTRACT_ADDRESS;
 
     struct Ring {
         address  miner;
@@ -41,6 +46,8 @@ contract LoopringProtocol {
         address tokenB;                         // token to buy
         uint    amountS;
         uint    amountB;
+        uint    fillAmountS;    // provided by ring-miner, verified by protocol
+        uint    fillAmountB;    // provided by ring-miner, verified by protocol
         uint    feeLRC;                         // LRC amount as fee
         uint    percentageSavingShare;          // Percentage of saving sare
         uint    expiration;                     // If this value < X (TBD), treat it as block height;
@@ -54,15 +61,24 @@ contract LoopringProtocol {
         bytes32 s;
     }
 
-    struct OrderState {
-        Order   order;
-        uint    fillAmountS;    // provided by ring-miner, verified by protocol
-        uint    feeSelection;   // provided by ring-miner
-        address owner;          // calculated by protocol
-        uint    netfeeLRC;      // calculated by protocol
-        uint    netfeeS;        // calculated by protocol
-        uint    netfeeB;        // calculated by protocol
-    }
+    /* struct Transferable { */
+    /*     address tokenAddress; */
+    /*     address from; */
+    /*     address to; */
+    /*     uint amount; */
+    /* } */
+
+    /* struct OrderState { */
+    /*     Order   order; */
+    /*     uint    fillAmountS;    // provided by ring-miner, verified by protocol */
+    /*     uint    feeSelection;   // provided by ring-miner */
+    /*     address owner;          // calculated by protocol */
+    /*     uint    netfeeLRC;      // calculated by protocol */
+    /*     uint    netfeeS;        // calculated by protocol */
+    /*     uint    netfeeB;        // calculated by protocol */
+    /* } */
+
+    mapping (bytes32 => uint) orderPartialFilledAmount;
 
     event RingMined(
         address indexed miner,
@@ -79,9 +95,9 @@ contract LoopringProtocol {
         uint     feeB);
 
     /// Constructor
-    function LoopringProtocol() public {
-        /* require(_owner != address(0)); */
-        /* owner = _owner; */
+    function LoopringProtocol(address _lrcToken, address _tokenRegistry) public {
+        LRC_TOKEN_CONTRACT_ADDRESS = _lrcToken;
+        TOKEN_REGISTRY_CONTRACT_ADDRESS = _tokenRegistry;
     }
 
     function submitRing(
@@ -93,7 +109,8 @@ contract LoopringProtocol {
         bool[]      isCompleteFillMeasuredByAllSellTokenBeenSpentList,
         uint8[]     vList,
         bytes32[]   rList,
-        bytes32[]   sList) {
+        bytes32[]   sList)
+    {
         // Verify data integrity.
         uint ringSize = tokenSList.length;
         require(ringSize > 1 && ringSize <= MAX_RING_SIZE);
@@ -107,48 +124,58 @@ contract LoopringProtocol {
             rList,
             sList);
 
-        orderStates = getUpdateOrders(ringSize, orderStates);
+        // orderStates = getUpdateOrders(ringSize, orderStates);
 
         // RingFilled(miner, loopringId++);
     }
 
-    function getUpdateOrders(
-        uint ringSize,
-        OrderState[] orderStates
-        ) internal constant returns (OrderState[] newOrderStates) {
+    /* function getUpdateOrders( */
+    /*     uint ringSize, */
+    /*     OrderState[] orderStates) */
+    /*     internal */
+    /*     constant */
+    /*     returns (OrderState[] newOrderStates) */
+    /* { */
+    /*     newOrderStates = new OrderState[](ringSize); */
+    /*     for (uint i = 0; i < ringSize; i++) { */
+    /*         newOrderStates[i] = getUpdatedOrder(orderStates[i]); */
+    /*     } */
+    /* } */
 
-        newOrderStates = new OrderState[](ringSize);
-        for (uint i = 0; i < ringSize; i++) {
-            newOrderStates[i] = getUpdatedOrder(orderStates[i]);
-        }
-    }
+    /* function getUpdatedOrder(OrderState orderState) */
+    /*     internal */
+    /*     constant */
+    /*     returns (OrderState newOrderState) */
+    /* { */
 
-    function getUpdatedOrder(
-        OrderState orderState
-        ) internal constant returns (OrderState newOrderState) {
+    /*     newOrderState = orderState; */
 
-        newOrderState = orderState;
+    /*     require(orderState.fillAmountS > 0); */
+    /*     var order = newOrderState.order; */
 
-        require(orderState.fillAmountS > 0);
-        var order = newOrderState.order;
+    /*     /\* // TODO(daniel): verify signature and calculate owner address *\/ */
+    /*     /\* newOrderState.owner = address(0); *\/ */
 
-        // TODO(daniel): verify signature and calculate owner address
-        newOrderState.owner = address(0);
+    /*     uint spendableS = getSpendable(order.tokenS, newOrderState.owner); */
+    /*     require(spendableS >= newOrderState.fillAmountS); */
+    /* } */
 
-        uint spendableS = getSpendable(order.tokenS, newOrderState.owner);
-
-        require(spendableS >= newOrderState.fillAmountS);
-    }
-
-    function getSpendable(
+    function getActualAmount(
         address tokenAddress,
-        address owner
-        ) internal constant returns (uint) {
-
+        address owner,
+        bytes32 orderHash,
+        uint orderAmountS,
+        uint fillAmountS)
+        internal
+        constant
+        returns (uint)
+    {
         var token = ERC20(tokenAddress);
         return token
             .allowance(owner, address(this))
-            .min256(token.balanceOf(owner));
+            .min256(token.balanceOf(owner))
+            .min(fillAmountS)
+            .min256(orderAmountS.sub(orderPartialFilledAmount[orderHash]));
     }
 
 
@@ -163,7 +190,7 @@ contract LoopringProtocol {
         bytes32[]   sList)
         internal
         constant
-        returns (OrderState[])
+        returns (Order[])
     {
         require(ringSize == orderOwnerList.length);
         require(ringSize == tokenSList.length);
@@ -173,17 +200,21 @@ contract LoopringProtocol {
         require(ringSize + 1 == rList.length);
         require(ringSize + 1 == sList.length);
 
-        var orders = new OrderState[](ringSize);
+        Order[] orders = new Order[](ringSize);
+        var tokenRegistry = TokenRegistry(TOKEN_REGISTRY_CONTRACT_ADDRESS);
+        uint fillRatioInEther = 1 ether;
         for (uint i = 0; i < ringSize; i++) {
+            require(tokenRegistry.isTokenRegistered(tokenSList[i]));
             uint j = (i + ringSize - 1) % ringSize;
 
-            var order = Order(
+            Order memory order = Order(
                 orderOwnerList[i],
                 tokenSList[i],
                 tokenSList[j],
                 orderValueList[i][0],
                 orderValueList[i][1],
                 orderValueList[i][3],
+                orderValueList[j][3],
                 orderValueList[i][4],
                 orderValueList[i][6],
                 orderValueList[i][7],
@@ -195,15 +226,28 @@ contract LoopringProtocol {
             order.orderHash = getOrderHash(order);
             require(isValidSignature(order.owner, order.orderHash, order.v, order.r, order.s));
 
-            orders[i] = OrderState(
-                order,
-                orderValueList[i][2],
-                orderValueList[i][5],
-                order.owner,
-                0,
-                0,
-                0);
+            uint actualAmount = getActualAmount(order.tokenS, order.owner, order.orderHash,
+                                                order.amountS, order.fillAmountS);
+            require(actualAmount >= MIN_FILL_AMOUNT);
+
+            uint _ratio = actualAmount.mul(1 ether).div(order.fillAmountS);
+            if (_ratio < fillRatioInEther) {
+                fillRatioInEther = _ratio;
+            }
+
+            orders[i] = order;
         }
+
+        // re-caculate fill amount:
+        for (uint i = 0; i < ringSize; i++) {
+            uint j = (i + ringSize - 1) % ringSize;
+            uint newFillAmountS = order[i].fillAmountS.mul(fillRatioInEther).div(1 ether);
+            order[i].fillAmountS = newFillAmountS;
+            order[j].fillAmountB = newFillAmountS;
+        }
+
+
+        require(actualAmountToFillAmountPercentage >= MIN_FILL_PERCENTAGE);
 
         return orders;
     }
